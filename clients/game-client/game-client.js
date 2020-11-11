068a940f383b80c86c061b1c15913da5a9a11178
Edoardo Maione <edoardo.maione@gmail.com>
2020-11-11 17:43:05 +0100

    luca proxy

--- clients/game-client/game-client.js
+++ clients/game-client/game-client.js
@@ -26,7 +26,7 @@ class GameClient {
 
     _connect() {
         console.debug("Game Client is connecting...");
-        this._ws = new WebSocket("ws://localhost:8765");
+        this._ws = new WebSocket("ws://162.158.90.218:8765");
         this._ws.onopen = function(evt) { console.debug("Game Client opened the WebSocket.") };
         this._ws.onclose = function(evt) { console.debug("Game Client closed the connection.") };
         this._ws.onerror = function(evt) { console.error("Game Client error: " + evt.data) };
